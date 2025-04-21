3e6a6ee8903c225ba3c5938c13bc2ba10e2d45ca
KutayErtug <kutayertug@gmail.com>
2025-04-21 21:39:30 +0300

    Created AdminPanel Branch

---
+++ config/connect_db.php
@@ -0,0 +1,16 @@
+<?php
+
+$host = "localhost";
+$dbname = "tiyatro";
+$root = "root";
+$pass = "";
+
+try {
+    $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $root, $pass);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Bağlantı hatası: " . $e->getMessage();
+    exit;
+}
+
+?>
